ccdba3fb77062681102643fb811a283e584e07a2
Manuel Martín <hola@manuelmartin.name>
2021-07-27 00:28:33 +0200

    Deleted API Key

--- utils/films.js
+++ utils/films.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const films = {
     
     getOneFilm: async (title) => {
-        const res = await fetch (`https://www.omdbapi.com/?t=${title}&apikey=baa1d554`);
+        const res = await fetch (`https://www.omdbapi.com/?t=${title}&apikey=####`);
         const film = await res.json();
         return film;
     }
